edeee9312d9085379812244b4fe051c60df6cc6d
camilojc <camilo@jcoder.io>
2017-09-02 22:13:30 +0100

    base files for the project

---
+++ src/main/java/io/jcoder/tutorial/Application.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2017 - JCoder Ltd
+ */
+package io.jcoder.tutorial;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Entry point for our app");
+    }
+
+}
