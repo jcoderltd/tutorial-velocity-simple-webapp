7d8d6fe11b88f1bbd38c0d26ad72f59bcf7fa429
camilojc <camilo@jcoder.io>
2017-09-04 21:31:47 +0100

    added name.vm template and forwarded processed nameServlet requests to it

--- src/main/java/io/jcoder/tutorial/NameServlet.java
+++ src/main/java/io/jcoder/tutorial/NameServlet.java
@@ -5,6 +5,7 @@ package io.jcoder.tutorial;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +21,13 @@ public class NameServlet extends HttpServlet {
 
         String processedName = String.format("%s %s %s", title, firstName, lastName).toUpperCase();
 
-        resp.getWriter().println(processedName);
+        // add attributes that can be used by the Velocity template (e.g. using $processedName)
+        req.setAttribute("processedName", processedName);
+        req.setAttribute("numberOfCharacters", processedName.length());
+
+        // forward the request to the Velocity template, using a dispatcher
+        // (this is really the main part that connects our View with the VelocityViewServlet)
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/name.vm");
+        dispatcher.forward(req, resp);
     }
 }
